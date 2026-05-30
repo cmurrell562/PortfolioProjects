19be289098e8b7010f9cf816d9ffd0fa5c88c4a1
cmurrell562 <abstract.car.car@gmail.com>
2026-05-30 18:09:40 -0500

    Update NashvilleHousingDataCleaning.sql
    Update comments

--- NashvilleHousingDataCleaning.sql
+++ NashvilleHousingDataCleaning.sql
@@ -23,6 +23,7 @@ FROM NashvilleHousing nh ;
 
 -------------------------------------------------
 -- Standarize Date Format %y/%m/%d from string
+-- Populate SaleDate_converted by using SaleDate in a date type format
 ALTER TABLE  NashvilleHousing 
 Add SaleDate_converted Date
 
@@ -34,9 +35,14 @@ SET SaleDate_converted = STR_TO_DATE(SaleDate, '%M %e, %Y')
 
 
 -- property that is being sold should not have null address
--- for every property record,find other records with the same ParcelID but is not the same row
+-- the mapping from parcel id to property address is constant, meaning if multiple records have the same parcel id
+	--they would have the same propery address. We can use this mapping to populate NULL property addresses when a parcel id exists
+	-- (self join on parcel id but its not the same row (do not join on same uniqueid)
+	-- for every property record,find other records with the same ParcelID but is not the same row
 -- NULLIF(a.propertyaddress, '') → turns empty strings into NULL
 -- COALESCE(..., b.propertyaddress) → if the first argument is now NULL, use b.propertyaddress
+--  Can verify NashvilleHousing was updated and all empty property addresses were populated by
+	-- running a query with WHERE nh.PropertyAddress = '' to see if anything was returned
 UPDATE NashvilleHousing nh
 JOIN NashvilleHousing nh2 
   ON nh.ParcelID = nh2.ParcelID 
@@ -46,17 +52,21 @@ WHERE nh.PropertyAddress = '';
 
 
 ----------------------------------------------------------------------
--- Breaking out Address into Individual Columns (ADdress, City, State)
+-- Breaking out Address into Individual Columns (Address, City, State)
 
+-- Add a VarChar columm
 ALTER TABLE  NashvilleHousing 
 Add PropertySplitAddress Varchar(255)
 
+-- Update the newly added column with values
+-- Return everything in PropertyAddress before the first comma
 UPDATE NashvilleHousing nh 
 SET PropertySplitAddress = SUBSTRING_INDEX(PropertyAddress, ',', 1)
 
 ALTER TABLE  NashvilleHousing 
 Add PropertySplitCity Varchar(255)
 
+-- Return everything in PropertyAddress after the first comma from the end
 UPDATE NashvilleHousing nh 
 SET PropertySplitCity = SUBSTRING_INDEX(PropertyAddress, ',', -1)
 
@@ -69,6 +79,8 @@ SET OwnerSplitAddress = SUBSTRING_INDEX(OwnerAddress, ',', 1)
 ALTER TABLE  NashvilleHousing 
 Add OwnerSplitCity Varchar(255)
 
+-- Return everything in PropertyAddress after the second comma from the end, then return everything before the fist comma
+	-- '123 Main St, Nashville, TN' --> Nashville
 UPDATE NashvilleHousing nh 
 SET OwnerSplitCity = SUBSTRING_INDEX(SUBSTRING_INDEX(OwnerAddress, ',', -2), ',',1)
 
